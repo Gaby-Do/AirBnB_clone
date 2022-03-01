e7d6d68a49cf747316a2c179534c2191cb6c0446
Gaby-Do <eme.gaby.do@gmail.com>
2022-03-01 13:15:35 -0800

    all documented

--- models/user.py
+++ models/user.py
@@ -12,11 +12,12 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-
+    """class User, inherits from BaseModel"""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
 
     def __str__(self):
+        """formats how __str__ will be printed"""
         return f'[{__class__.__name__}] ({self.id}) {self.__dict__}'
